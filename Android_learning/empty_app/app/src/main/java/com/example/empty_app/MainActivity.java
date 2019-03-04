9e769c8b54129734b24616c54557cb8d3b11190b
u6462764 <dsh15325@163.com>
2019-03-04 15:43:04 +1100

    from scratch in empty_app

--- Android_learning/empty_app/app/src/main/java/com/example/empty_app/MainActivity.java
+++ Android_learning/empty_app/app/src/main/java/com/example/empty_app/MainActivity.java
@@ -1,17 +1,52 @@
 package com.example.empty_app;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
+import android.util.Log;
 
-public class MainActivity {
+public class MainActivity extends Activity {
+    String msg = "Android : ";
+
+    /** 当活动第一次被创建时调用 */
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
+    }
+
+    /** 当活动即将可见时调用 */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
+    /** 当活动可见时调用 */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    /** 当其他活动获得焦点时调用 */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    /** 当活动不再可见时调用 */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /** 当活动将被销毁时调用 */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
     }
 }
